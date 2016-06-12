8fd1c416c23f5e5b47b89a132818c245b4dfe583
sauja <sauja7@gmail.com>
2016-06-12 01:32:17 -0500

    ChocolateFeast

--- src/HackerRank/Algorithms/ChocolateFeast.java
+++ src/HackerRank/Algorithms/ChocolateFeast.java
@@ -5,5 +5,27 @@ package HackerRank.Algorithms;
  */
 public class ChocolateFeast {
     /*
+         Scanner in = new Scanner(System.in);
+        int n=in.nextInt();
+        for(int i=0;i<n;i++)
+            test(in.nextInt(),in.nextInt(),in.nextInt());
+
+
+    }
+
+    private static void test(int n,int c,int m) {
+        int totalChocolates=n/c;
+        int w=n/c;
+        while(Math.floor(w/m)!=0)
+        { //System.out.println("1:"+totalChocolates+" "+w);
+            totalChocolates+=w/m;
+            int currWrappers=w/m;
+            w=w%m+currWrappers;
+            //System.out.println("2:"+totalChocolates+" "+w);
+        }
+        System.out.println(totalChocolates);
+    }
+
+}
      */
 }
--- src/HackerRank/Algorithms/Solution.java
+++ src/HackerRank/Algorithms/Solution.java
@@ -7,14 +7,23 @@ public class Solution {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n=in.nextInt();
-        int sticks[]=new int[n];
         for(int i=0;i<n;i++)
-            sticks[i]=in.nextInt();
-        test(n,sticks);
+            test(in.nextInt(),in.nextInt(),in.nextInt());
+
 
     }
 
-    private static void test(int n,int sticks[]) {
+    private static void test(int n,int c,int m) {
+        int totalChocolates=n/c;
+        int w=n/c;
+        while(Math.floor(w/m)!=0)
+        { //System.out.println("1:"+totalChocolates+" "+w);
+            totalChocolates+=w/m;
+            int currWrappers=w/m;
+            w=w%m+currWrappers;
+            //System.out.println("2:"+totalChocolates+" "+w);
+        }
+        System.out.println(totalChocolates);
     }
 
 }
